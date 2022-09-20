7e930150b7670bc365126f13e2a4299298d55307
ikism <amlxss@163.com>
2022-09-20 11:23:03 +0800

    fix commentary

--- index.js
+++ index.js
@@ -8,57 +8,85 @@ const excelPath = './挤出机采集系统多语言.xls';
 // let jsonPath = 'F:/theSaltWater/xzlsjq-oms-mall-web/src/locales';
 // let jsonPath = 'F:/theSaltWater/xzlsjq-oms-mall-h5/i18n';
 let locales = [];
-var walkDir = function (pathStr) {
+var readJSONDir = function (pathStr) {
     fs.readdir(pathStr, function (err, files) {
         files.filter(f => f.includes('.json')).forEach(function (file) {
             locales.push(require(pathStr + '/' + file));
         });
         console.log('locales',locales);
-        managerJSON();
+        jsonToExcel();
     });
 }
-// walkDir(jsonPath);
-var managerJSON = function(){
+// readJSONDir(jsonPath);
+var jsonToExcel = function(){
     if(!locales.length){
         console.log('没有数据');
         return;
     }
-    // 扁平
     let _locales = [];
     locales.forEach(lan =>{
-        let obj = getFlatObj(lan)
-        _locales.length = obj.size;
+        // 扁平化json {a:{b:1}} => Map([a.b]:1)
+        let objMap = getFlatObj(lan)
+        _locales.length = objMap.size;
         let i = 0;
-        obj.forEach((value,key) =>{
+        objMap.forEach((value,key) =>{
             if(!_locales[i]){
                 _locales[i] = [key];
             } 
-            _locales[i].push(obj.get(_locales[i][0]));
+            _locales[i].push(objMap.get(_locales[i][0]));
             i++;
         })
     })
-    console.log('_locales',_locales);
+    console.log('_locales',_locales); // [[key,l1,l2,l3],[key,l1,l2,l3],...]
     fs.writeFile(excelPath,xlsx.build([{name: 'first', data: _locales}]),function(error){
         if(error){
             console.log('error',error);
         }
     })
 }
-
+/**
+ * excel 
+ * sheet 1
+ * key l1 l2 l3
+ * 单工作表
+ */
 var excelToJson = function(){
     var xlsList = xlsx.parse(excelPath).map(sheet => sheet.data);
     let objList = [];
-    objList.length = xlsList[0][0].length;
+    objList.length = xlsList[0][0].length-1;
     objList.fill(null);
     xlsList[0].forEach((values,pIndex) =>{
-        let key = xlsList[1][pIndex][0];
+        let key = values.shift();
         objList.forEach((obj,index) =>{
             if(!obj)(obj = objList[index] = {});
             obj[key] = values[index];
         })
     })
+    writeJSON(JsonList);
+}
+/**
+ * excel 
+ * sheet 1      sheet2
+ * l1 l2 l3     key
+ * 双工作表
+ */
+var excelToJsonByDoubleSheet = function(){
+    var xlsList = xlsx.parse(excelPath).map(sheet => sheet.data);
+    let JsonList = [];
+    JsonList.length = xlsList[0][0].length;
+    JsonList.fill(null);
+    xlsList[0].forEach((values,pIndex) =>{
+        let key = xlsList[1][pIndex][0];
+        JsonList.forEach((obj,index) =>{
+            if(!obj)(obj = JsonList[index] = {});
+            obj[key] = values[index];
+        })
+    })
+    writeJSON(JsonList);
+}
+var writeJSON = function(JsonList){
     // console.log('objList[0]',objList);
-    objList.forEach((obj,pIndex) =>{
+    JsonList.forEach((obj,pIndex) =>{
         fs.writeFile(`./${pIndex}.json`,JSON.stringify(resetKey(obj),null,4),function(error){
             if(error){
                 console.log('error',error);
---
+++ readme.md
@@ -0,0 +1,3 @@
+# I18n-json-excel
+
+json 多语言文件与excel 互转 方便导入导出，翻译校对
